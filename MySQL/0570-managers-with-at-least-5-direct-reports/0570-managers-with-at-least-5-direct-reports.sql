df81026f34a5cf273ffabc7bb37aabfc5efb764b
Aditi Rawat <adsrawat2001@gmail.com>
2026-07-21 12:05:35 +0530

    Time: 360 ms (53.12%), Space: 0B (100%) - LeetHub

--- MySQL/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ MySQL/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,6 @@
 # Write your MySQL query statement below
 select 
-case 
-    when e1.name is null then  null
-    else e1.name
-end as name
+e1.name
 from employee e1
 inner join employee e2
 on e1.id=e2.managerId
