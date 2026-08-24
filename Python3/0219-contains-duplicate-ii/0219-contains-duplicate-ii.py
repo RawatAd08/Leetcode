d586243b7c965e2e62221b42dac1fe1d8203db0b
Aditi Rawat <adsrawat2001@gmail.com>
2026-08-24 20:37:15 +0530

    Time: 66 ms (9.8%), Space: 39.2 MB (28.82%) - LeetHub

--- Python3/0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ Python3/0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,28 +1,25 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        n=len(nums)
-        d={}
-        for i in range(min(k+1, n)):          # <-- CHANGED: prevent IndexError when n < k+1
+        n = len(nums)
+        d = {}
+        for i in range(min(k+1, n)):
             if nums[i] not in d:
-                d[nums[i]]=i
+                d[nums[i]] = i
             else:
-                prev_idx=d.get(nums[i])
-                if abs(i-prev_idx)<=k:
+                prev_idx = d.get(nums[i])
+                if abs(i - prev_idx) <= k:
                     return True
-    
-        for i in range(min(k+1, n),n):        # <-- CHANGED: match the same bound as above
-            left=nums[i-k-1]
-            new=nums[i]
-            #remove element
-            del(d[left])
-            
-            if new in d:
-                prev_idx=d.get(new)
-                if abs(i-prev_idx)<=k:
-                    return True
-                d[new]=i                       # <-- ADDED: refresh stale index when NOT returning True
-            else: #add element
-                d[new]=i
 
-            
+        for i in range(k+1, n):          # <-- fixed range
+            left = nums[i-k-1]
+            new = nums[i]
+            del d[left]                  # remove element leaving the window
+
+            if new in d:
+                prev_idx = d.get(new)
+                if abs(i - prev_idx) <= k:
+                    return True
+            else:
+                d[new] = i
+
         return False
